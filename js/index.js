926829c36662f16c5893ee681942b65917a1ac6b
Himalaya <yugmani@yahoo.com>
2020-04-21 23:02:14 -0700

    partially done

---
+++ js/index.js
@@ -0,0 +1,46 @@
+
+var map;
+var markers = [];
+var infoWindow;
+var locationSelect;
+
+function initMap() {
+    var losangeles = {
+        lat: 34.063380, 
+        lng: -118.358080
+    };
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: losangeles,
+      zoom: 11,
+      mapTypeId: 'roadmap',
+    //   mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU}
+    });
+
+    showStoreMarkers();
+}
+
+
+
+function showStoreMarkers() {
+    // console.log(stores);
+    stores.forEach(function(store, index){
+       
+         var latlng = new google.maps.LatLng(
+             store.coordinates.latitude,
+             store.coordinates.longitude);
+        var name = store.name;
+        var address = store.addressLines[0];
+        createMarker(latlng, name, address);
+    })
+}
+
+function createMarker(latlng, name, address){
+    var html = "<b>" + name+ "</b> <br />" + address;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: latlng
+    });
+
+    markers.push(marker);
+}
+
