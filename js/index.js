f146da6c0a76bee23fb507915ab0e3fb7b1c4768
Himalaya <yugmani@yahoo.com>
2020-06-08 19:30:14 -0700

    store locator done

--- js/index.js
+++ js/index.js
@@ -5,7 +5,6 @@ var infoWindow;
 var locationSelect;
 
 
-
 //function to initialize the map
 function initMap() {
     var losangeles = {
@@ -14,29 +13,70 @@ function initMap() {
     };
     map = new google.maps.Map(document.getElementById('map'), {
       center: losangeles,
-      zoom: 11,
-      mapTypeId: 'roadmap',
-    //   mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU}
+      zoom: 50,
     });
 
+    infoWindow = new google.maps.InfoWindow();
+    //call displayStores function
+    displayStores();
+    //call showStoreMarkers function
     showStoreMarkers();
+}
+
+
+function displayStores() {
+    var storesHtml = "";
+    stores.forEach(function(store, index){
+        // console.log(store);
+        var address = store.addressLines;
+        var phone = store.phoneNumber;
+        console.log(address.length);
+        storesHtml +=
+        `<div class="store-container">
+        <div class="store-info-container">
+            <div class="store-address">`;
+        
+            if (address.length>2){
+                storesHtml += `<span>${address[0]} ${address[1]}</span>
+                <span>${address[2]}</span>`;
+            }
+            else {
+                storesHtml += `<span>${address[0]}</span>
+                <span>${address[1]}</span>`;
+            }
+                // <!-- <i class="fas fa-map-marker-alt"></i> -->
+                storesHtml += `</div>
+            <div class="store-phone-number"><i class="fas fa-phone-volume"></i>  ${phone}</div>
+        </div>
+            <div class="store-number-container">
+                <div class="store-number">
+                   ${index+1}
+                </div>
+            </div>
+    </div>`;
+
+    });
+    document.querySelector('.stores-list').innerHTML = storesHtml;
 }
 
 
 //function to show store markers
 function showStoreMarkers() {
-    // console.log(stores);
-    stores.forEach(function(store, index){
-       
-         var latlng = new google.maps.LatLng(
-             store.coordinates.latitude,
-             store.coordinates.longitude);
-        var name = store.name;
-        var address = store.addressLines[0];
-        createMarker(latlng, name, address);
-    })
-}
+    var bounds = new google.maps.LatLngBounds();
+        stores.forEach(function(store, index){
+            // console.log(stores);
+            var latlng = new google.maps.LatLng(
+                        store.coordinates.latitude,
+                        store.coordinates.longitude);
+                // console.log(latlng);
+            var name = store.name;
+            var address = store.addressLines[0];
 
+            bounds.extend(latlng);
+            createMarker(latlng, name, address);
+    })
+    map.fitBounds(bounds);
+}
 
 //function to create markers
 function createMarker(latlng, name, address){
@@ -45,6 +85,11 @@ function createMarker(latlng, name, address){
         map: map,
         position: latlng
     });
+
+    google.maps.event.addListener(marker, 'mouseover', function() {
+        infoWindow.setContent(html);
+        infoWindow.open(map, marker);
+      });
 
     markers.push(marker);
 }
